25a8f50d51a7d76eae7f9f4b6a48c40c0ee9fdf9
yashikadilliwal <yashiikpop@gmail.com>
2026-05-30 14:11:13 +0530

    feat: added more models

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from autoslug import AutoSlugField
 
 # Create your models here.
@@ -27,3 +28,40 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+class Cart(models.Model):
+        user=models.OneToOneField(User, on_delete=models.CASCADE)
+        created_at=models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product=models.ForeignKey(Product, on_delete=models.CASCADE,  related_name='product')
+    quantity=models.PositiveIntegerField()
+
+   
+
+class Order(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price=models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OrderItem(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+    ]
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.PositiveIntegerField()
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
