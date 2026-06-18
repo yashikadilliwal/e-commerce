65414f13aa366bb759d66d9567ce8792102e84c3
yashikadilliwal <yashiikpop@gmail.com>
2026-06-18 14:43:44 +0530

    feat : add build

--- core/settings.py
+++ core/settings.py
@@ -30,9 +30,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-900+3(*6j%6-^8(^jk8a)fsudu#svt1lm^g4nj3@l*scls*0*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
